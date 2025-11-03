549e544685e6975afdc8a0cc007853d27f71b0e0
vmeggiolaro <vmeggiolaro@udesa.edu.ar>
2025-11-03 10:56:57 -0300

    ahora si hecha la navegacion basica

--- TP2/src/componentes/HomeMenu.js
+++ TP2/src/componentes/HomeMenu.js
@@ -2,19 +2,20 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Profile from "../screens/Profile";
 import Comments from "../screens/Comments"
 import HomePage from "../screens/HomePage"
-import CommentPost from "../screens/ComentPost"
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
+import Entypo from '@expo/vector-icons/Entypo';
+import Feather from '@expo/vector-icons/Feather';
 
 const Tab = createBottomTabNavigator();
 
 export default function HomeMenu() {
   return (
         <Tab.Navigator>
-          <Stack.Screen name="Profie" component={Profile} options={ {headerShown: false} }/>
-          <Stack.Screen name="Comments" component={Comments} options={ {headerShown: false} }/>
-          <Stack.Screen name="HomePage" component={HomePage} options={ {headerShown: false} }/>
-          <Stack.Screen name="CommentPost" component={CommentPost} options={ {headerShown: false} }/>
+          <Tab.Screen name="Profie" component={Profile} options={ {headerShown: false,
+            tabBarIcon: () => <Feather name="user" size={24} color="black" /> } }/>
+          <Tab.Screen name="Comments" component={Comments} options={ {headerShown: false, 
+            tabBarIcon: () => <Feather name="plus-square" size={24} color="black" />} }/>
+          <Tab.Screen name="HomePage" component={HomePage} options={ {headerShown: false, 
+            tabBarIcon: () => <Entypo name="home" size={24} color="black" />} }/>
         </Tab.Navigator>
   );
 }
